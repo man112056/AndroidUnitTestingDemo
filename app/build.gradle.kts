c6abd7758c8e68b9edab1e9b75571a64117aaa5b
Manish <man112056@gmail.com>
2025-01-06 18:06:14 +0530

    added xml view in jetpack compose project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,14 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // dependencies from xml based activity starts
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    // dependencies from xml based activity ends
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
